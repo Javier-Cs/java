3662b66089066614a9139d6ee5712db47b6d4850
Javier-Cs <krchipullajavier@gmail.com>
2025-05-30 14:55:12 -0500

    implementacion de cambios a servicio carro

---
+++ ServicioDeUsuario/src/main/java/com/prueba/ServicioDeUsuario/Models/CarroModel.java
@@ -0,0 +1,9 @@
+package com.prueba.ServicioDeUsuario.Models;
+
+import lombok.Data;
+
+@Data
+public class CarroModel {
+    private String marca;
+    private String modelo;
+}
---
+++ ServicioDeUsuario/src/main/java/com/prueba/ServicioDeUsuario/Models/MotoModel.java
@@ -0,0 +1,10 @@
+package com.prueba.ServicioDeUsuario.Models;
+
+import lombok.Data;
+
+@Data
+public class MotoModel {
+
+    private String modelo;
+    private String marca;
+}
